c62a31dd37cc57323166515526e414d749771885
Maël Quéau <mael.queau99@gmail.com>
2023-02-03 22:15:18 +0100

    feat(typescript): resistor color

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,16 @@
+export const colorCode = (c: string): number => {
+  return COLORS.indexOf(c);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];

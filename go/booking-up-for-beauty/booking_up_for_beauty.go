b80919c08e6155d5807c1f1721880c4c954e8333
Maël Quéau <mael.queau99@gmail.com>
2023-08-28 15:31:21 +0200

    feat(go): booking up for beauty

---
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,46 @@
+package booking
+
+import (
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+	format := "1/2/2006 15:04:05"
+
+	t, _ := time.Parse(format, date)
+
+	return t
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	format := "January 2, 2006 15:04:05"
+
+	t, _ := time.Parse(format, date)
+
+	return t.Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+	format := "Monday, January 2, 2006 15:04:05"
+
+	t, _ := time.Parse(format, date)
+
+	return t.Hour() >= 12 && t.Hour() < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	format := "1/2/2006 15:04:05"
+
+	t, _ := time.Parse(format, date)
+
+	return "You have an appointment on " + t.Format("Monday, January 2, 2006, at 15:04.")
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+	return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)
+}

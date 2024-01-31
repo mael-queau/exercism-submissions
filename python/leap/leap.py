e9a4b4d2fe8f6e015174219d7afde588af226607
Maël Quéau <mael.queau99@gmail.com>
2024-01-31 22:22:22 +0100

    feat(python): leap v2

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,6 +1,2 @@
-"""Is that a leap year?
-"""
-
-
 def leap_year(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    return year % 4 == 0 and not (year % 100 == 0 and year % 400 != 0)
